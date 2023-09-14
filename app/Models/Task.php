cab1c030bc6cd9a579c603b8e9b7594b3a2330ca
douglasew <douglas_ewerton10@hotmail.com>
2023-09-14 20:47:24 -0300

    add task model

---
+++ app/Models/Task.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+        'status',
+        'finish_date'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
