56cb7560692e45f0d6f3fbbdce21fe54e710d097
douglasew <douglas_ewerton10@hotmail.com>
2023-09-14 20:18:37 -0300

    add photo validation

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -18,6 +18,21 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['string', 'max:255'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'photo' => [
+                'image',
+                'mimes:jpg,png',
+                'max:1024',
+                'dimensions:min_width=500,min_height=500,max_width=500,max_height=500'
+            ]
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'photo.max' => 'Tamanho máximo da imagem é de 1M',
+            'photo.mimes' => 'Formato incorreto',
+            'photo.dimensions' => 'Dimensão 500 x 500'
         ];
     }
 }
