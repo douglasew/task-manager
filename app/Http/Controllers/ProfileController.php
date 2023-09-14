3d3ae5f2358b0d7ab6db78ff700e41fb46af9fc0
douglasew <douglas_ewerton10@hotmail.com>
2023-09-14 20:25:42 -0300

    add upload photo

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -28,6 +28,10 @@ class ProfileController extends Controller
     {
         $request->user()->fill($request->validated());
 
+        if ($request->photo) {
+            $data['photo'] = $request->photo->store('users');
+        }
+
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
