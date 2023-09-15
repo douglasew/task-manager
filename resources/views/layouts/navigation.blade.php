4fff8ebd0fe365c63a8797a8bc66d58b5e7a814f
douglasew <douglas_ewerton10@hotmail.com>
2023-09-14 22:34:56 -0300

    add task tab in navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('create-task')" :active="request()->routeIs('create-task')">
+                        {{ __('Criar tarefa') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
