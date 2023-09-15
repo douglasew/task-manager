3bf7dc93292d0bb9a3fcd9648c3958928ecd34af
douglasew <douglas_ewerton10@hotmail.com>
2023-09-14 21:24:44 -0300

    add task authentication

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,5 +26,19 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Gate::define('view-task', function (User $user, Task $task) {
+            if ($user->id === $task->user_id) {
+                return true;
+            }
+            //return $user->id === $task->user_id and Arr::exists($task, $task->id);
+        });
+
+        Gate::define('task-exists', function ($data) {
+            $task = Task::find($data->id);
+
+            if (!$task) {
+                return false;
+            }
+        });
     }
 }
