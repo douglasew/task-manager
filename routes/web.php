943e560b5583ee8273490f7cc47699b071b87229
douglasew <douglas_ewerton10@hotmail.com>
2023-09-14 22:26:54 -0300

    add tasks routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,14 +19,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get("/dashboard", [TaskController::class, "index"])
+    ->middleware(["auth", "verified"])
+    ->name("dashboard");
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::prefix('profile')->group(function () {
+        Route::controller(ProfileController::class)->group(function () {
+            Route::get('/', 'edit')->name('profile.edit');
+            Route::patch('/', 'update')->name('profile.update');
+            Route::delete('/', 'destroy')->name('profile.destroy');
+        });
+    });
+
+    Route::prefix('task')->group(function () {
+        Route::controller(TaskController::class)->group(function () {
+            Route::get('/', 'create')->name('create-task');
+            Route::post('/', 'store')->name('task.create');
+            Route::get('/{id}', 'edit')->name('task.edit');
+            Route::patch('/{id}', 'update')->name('task.update');
+            Route::delete('/{id}', 'destroy')->name('task.destroy');
+        });
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
